31d24d7c833f4f5ecd09c7e88a5235b355abd0c0
Thanh Vu <thanhauco@gmail.com>
2021-01-15 18:02:00 -0800

    Format PDF Table

--- lib/pdf_generator.rb
+++ lib/pdf_generator.rb
@@ -1,9 +1,11 @@
 require 'prawn'
+require 'prawn/table'
 class PdfGenerator
   def generate(data, filename)
     Prawn::Document.generate(filename) do
-      text 'Report'
-      data.each { |row| text row.inspect }
+      text 'Report', size: 20, style: :bold
+      move_down 20
+      table([data.first.keys] + data.map(&:values))
     end
   end
 end
