be329fc8c66ab9834d4b676e4cd5a4d3c2b9c644
Thanh Vu <thanhauco@gmail.com>
2021-01-21 21:54:00 -0800

    Integrate in Main

--- main.rb
+++ main.rb
@@ -1,1 +1,7 @@
-puts 'Report Gen'
+require_relative 'lib/data_source'
+require_relative 'lib/pdf_generator'
+require_relative 'lib/csv_generator'
+data = DataSource.new.fetch
+PdfGenerator.new.generate(data, 'output/report.pdf')
+CsvGenerator.new.generate(data, 'output/report.csv')
+puts 'Generated'
