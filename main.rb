16e38762c4d87549d012918348a9e3d0b8bca5e0
Thanh Vu <thanhauco@gmail.com>
2021-01-30 01:37:00 -0800

    Schedule Job

--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
+require 'rufus-scheduler'
 require_relative 'lib/data_source'
 require_relative 'lib/pdf_generator'
-require_relative 'lib/csv_generator'
 require_relative 'lib/emailer'
-data = DataSource.new.fetch
-PdfGenerator.new.generate(data, 'output/report.pdf')
-Emailer.new.send('output/report.pdf', 'admin@example.com')
-puts 'Done'
+scheduler = Rufus::Scheduler.new
+scheduler.every '1m' do
+  data = DataSource.new.fetch
+  PdfGenerator.new.generate(data, 'output/report.pdf')
+  Emailer.new.send('output/report.pdf', 'admin@example.com')
+  puts 'Job run'
+end
+scheduler.join
