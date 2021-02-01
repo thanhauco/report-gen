30698c766497e6713b14a6a2a6b4e9c8fadeed94
Thanh Vu <thanhauco@gmail.com>
2021-02-01 12:55:00 -0800

    Use Config

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require 'rufus-scheduler'
+require_relative 'lib/config'
 require_relative 'lib/data_source'
 require_relative 'lib/pdf_generator'
 require_relative 'lib/emailer'
+config = Config.load
 scheduler = Rufus::Scheduler.new
-scheduler.every '1m' do
+scheduler.every config['schedule'] do
   data = DataSource.new.fetch
   PdfGenerator.new.generate(data, 'output/report.pdf')
-  Emailer.new.send('output/report.pdf', 'admin@example.com')
+  Emailer.new.send('output/report.pdf', config['email'])
   puts 'Job run'
 end
 scheduler.join
