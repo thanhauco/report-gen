f50cd621457be0052325909ad5cdc9d890a0833a
Thanh Vu <thanhauco@gmail.com>
2021-03-06 15:40:00 -0800

    Add Webhook Trigger

--- main.rb
+++ main.rb
@@ -1,14 +1,5 @@
-require 'rufus-scheduler'
-require_relative 'lib/config'
-require_relative 'lib/data_source'
-require_relative 'lib/pdf_generator'
-require_relative 'lib/emailer'
-config = Config.load
-scheduler = Rufus::Scheduler.new
-scheduler.every config['schedule'] do
-  data = DataSource.new.fetch
-  PdfGenerator.new.generate(data, 'output/report.pdf')
-  Emailer.new.send('output/report.pdf', config['email'])
-  puts 'Job run'
+require 'sinatra'
+post '/trigger' do
+  # Trigger report generation
+  "Triggered"
 end
-scheduler.join
